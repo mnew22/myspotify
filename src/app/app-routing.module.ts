59459d4061bf524a19923f9e181a2431d0b453b8
mnew22 <mnew700222@gmail.com>
2025-02-24 09:14:19 +0100

    leccion 18a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
+import { HomeModule } from './modules/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:() => import('./modules/home/home.module').then(m =>m.HomeModule)
+    
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+
+  linksMenu: Array<any> = [
+    {
+      name: 'Home',
+      icon: '',
+
+    }
+    {
+      name: 'Buscar',
+      icon: '',
+      
+    }
+  ];
+
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 
 }
